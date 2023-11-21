0b5d410209dcf6e0de5f659c64f2abd300209554
TeodorLevakov <37295419+TeodorLevakov@users.noreply.github.com>
2023-11-21 13:35:26 +0100

    modify requester

--- client/src/lib/request.js
+++ client/src/lib/request.js
@@ -1,9 +1,29 @@
-export const request = async (method, url, data) => {
+
+const buildOptions = (data) => {
+    const options = {};
+
+    if (data) {
+       options.body = JSON.stringify(data); 
+       options.headers = {
+        'Content-Type': 'applications/json'
+       };
+    }
+
+    return options;
+}
+
+ const request = async (method, url, data) => {
     const responce = await fetch(url, {
+        ...buildOptions(data),
         method,
     });
 
     const result = await responce.json();
 
     return result;
-}
+};
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const remove = request.bind(null, 'DELETE');
--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -1,24 +1,26 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const baseUrl = 'http://localhost:3030/jsonstore/games';
 
 
 export const getAll = async () => {
-    const response = await request('GET', baseUrl);
+    const response = await request.get(baseUrl);
 
     return Object.values(response);
-}
+};
 
 export const create = async (data) => {
-    const response = await fetch(baseUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
+    // const response = await fetch(baseUrl, {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(data)
+    // });
 
-    const result = await response.json();
+    // const result = await response.json();
+
+    const result = await request.post(baseUrl, data);
 
     return result;
 };
