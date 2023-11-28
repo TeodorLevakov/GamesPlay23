88293b0cae0366950752b09606e4d33dbec8b074
TeodorLevakov <37295419+TeodorLevakov@users.noreply.github.com>
2023-11-28 18:08:55 +0100

    Add useForm hook

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -3,9 +3,12 @@ import * as request from '../lib/request';
 const baseUrl = 'http://localhost:3030/jsonstore/comments';
 
 export const getAll = async (gameId) => {
-    const result = await request.get(baseUrl);
 
-    return Object.values(result).filter(c => c.gameId === gameId);
+    const query = new URLSearchParams({where: `gameId="${gameId}"`})
+
+    const result = await request.get(`${baseUrl}?${query}`);
+
+    return Object.values(result);
 }
 
 
